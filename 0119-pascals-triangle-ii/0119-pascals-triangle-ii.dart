fec3458f36b4b4e6cca2ba9eb0f4d9f0d89f7a96
Muhammed Midlaj <midlajkk04@gmail.com>
2026-05-14 10:56:14 +0530

    Time: 0 ms (100%), Space: 150.3 MB (28.57%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.dart
@@ -0,0 +1,13 @@
+class Solution {
+  List<int> getRow(int rowIndex) {
+    List<int> result = [];
+    int value = 1;
+
+    for (int i = 0; i <= rowIndex; i++) {
+      result.add(value);
+      value = (value * (rowIndex - i)) ~/ (i + 1);
+    }
+
+    return result;
+  }
+}
