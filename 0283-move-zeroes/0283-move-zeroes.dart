c582691c00d86f6f7b75ab18650fdfd02d0e4fcd
Muhammed Midlaj <midlajkk04@gmail.com>
2026-07-16 16:23:45 +0530

    Time: 8 ms (24.58%), Space: 156.1 MB (41.53%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.dart
@@ -0,0 +1,9 @@
+class Solution {
+  void moveZeroes(List<int> nums) {
+      List<int> testValues = [];
+      nums.forEach((value)=> testValues.add(value));
+      testValues.retainWhere((value)=> value == 0);
+      nums.removeWhere((value)=> value == 0);
+      nums.addAll(testValues);
+  }
+}
